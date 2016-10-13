eaf6935d58f837913311f9fc4cd5e45e6adb8d59
Roberto Carlos Flores Gómez <Roberto Carlos Flores Gmez>
2016-10-13 11:05:23 +0200

    Ejercicios 8-10

--- ejercicios8-10/js/ejercicio8.js
+++ ejercicios8-10/js/ejercicio8.js
@@ -1,11 +1,10 @@
 var numero = prompt("Introduce un número entero");
-var resultado = parImpar(numero);
-alert("El número "+numero+" es "+resultado);
-function parImpar(numero) {
-    if(numero % 2 == 0) {
-        return "par";
+alert("El número " + numero + " es " + ((esPar(numero)) ? "par" : "impar"));//? si es true y : si es false
+function esPar(numero) {
+    if (numero % 2 == 0) {
+        return true;
     }
     else {
-        return "impar";
+        return false;
     }
 }
